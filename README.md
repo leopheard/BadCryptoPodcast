6dff87a61563f55691d1cab75d7b862551ca3729
leopheard <leopheard.misc@gmail.com>
2021-02-28 19:56:46 -0500

    upload

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-<a href="kodi.tv">KODI<a> (XBMC/OSMC/Firestick) audio addon for the <a href="">xxxxxxxxxx</a> podcast.<br>
+<a href="kodi.tv">KODI<a> (XBMC/OSMC/Firestick) audio addon for the <a href="https://badcryptopodcast.com/">Bad Crypto Podcast</a>.<br>
 
-<img src=""><br>
+<img src="https://badcryptopodcast.com/wp-content/uploads/2018/06/bad-crypto-logo.png"><br>
+
+The podcast for the crypto-curious and the crypto-serious. We’re the blockchain blockheads, the crypto clowns -- Duck Duck Going stuff so you don’t have to -- Once you go BAD, you never go back.<br>
+
+- The Bad Crypto Podcast<br>
+- The Crypto Chick Podcast<br>
+- The Bad Boys Of Tech<br>
 
 <a href="www.kodi.tv">Kodi</a> (formerly known as XBMC) is an award-winning free and open source (GPL) software media player and entertainment hub that can be installed on Linux, OSX, Windows, iOS and Android, featuring a 10-foot user interface for use with televisions and remote controls. It allows users to play and view most videos, music, podcasts, and other digital media files from local and network storage media and the internet.<br>
 
--- addon.py
+++ addon.py
@@ -1,19 +1,24 @@
 from xbmcswift2 import Plugin, xbmcgui
 from resources.lib import mainaddon
-
 plugin = Plugin()
-url1 = ""
+url1 = "https://omny.fm/shows/badcrypto/playlists/podcast.rss"
+url2 = "https://omny.fm/shows/cryptochick/playlists/podcast.rss"
+url3 = "https://www.omnycontent.com/d/playlist/8edea6b9-fca4-41a1-83ee-aa76002b9dd8/c49b612f-7eb3-48d3-bde6-aa8c00ed740f/c925d231-cd4d-4ab0-9901-aa8c00ed742c/podcast.rss"
 @plugin.route('/')
 def main_menu():
     items = [
         {
-            'label': plugin.get_string(30001), 
+            'label': plugin.get_string(30001),
             'path': plugin.url_for('episodes1'),
-            'thumbnail': ""},
+            'thumbnail': "https://www.omnycontent.com/d/playlist/8edea6b9-fca4-41a1-83ee-aa76002b9dd8/8f677bef-192a-4471-a50b-abcd002a30ee/5ec27e5e-3d28-4913-b240-abcd002a3106/image.jpg"},
         {
-            'label': plugin.get_string(30000),
-            'path': plugin.url_for('episodes'),
-            'thumbnail': ""},
+            'label': plugin.get_string(30002),
+            'path': plugin.url_for('episodes2'),
+            'thumbnail': "https://podcastaddict.com/cache/artwork/thumb/2314341"},
+        {
+            'label': plugin.get_string(30003),
+            'path': plugin.url_for('episodes3'),
+            'thumbnail': "https://podcastaddict.com/cache/artwork/thumb/2441877"},
     ]
     return items
 
@@ -23,12 +28,17 @@ def episodes1():
     playable_podcast1 = mainaddon.get_playable_podcast1(soup1)
     items = mainaddon.compile_playable_podcast1(playable_podcast1)
     return items
-
-@plugin.route('/episodes/')
-def episodes():
-    soup1 = mainaddon.get_soup1(url1)
-    playable_podcast = mainaddon.get_playable_podcast(soup1)
-    items = mainaddon.compile_playable_podcast(playable_podcast)
+@plugin.route('/episodes2/')
+def episodes2():
+    soup2 = mainaddon.get_soup2(url2)
+    playable_podcast2 = mainaddon.get_playable_podcast2(soup2)
+    items = mainaddon.compile_playable_podcast2(playable_podcast2)
+    return items
+@plugin.route('/episodes3/')
+def episodes3():
+    soup3 = mainaddon.get_soup3(url3)
+    playable_podcast3 = mainaddon.get_playable_podcast3(soup3)
+    items = mainaddon.compile_playable_podcast3(playable_podcast3)
     return items
 
 if __name__ == '__main__':
